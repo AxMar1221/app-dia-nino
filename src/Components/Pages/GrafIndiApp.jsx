8e260373bd3d6211f3aa1379d4f6c99352a6cfa8
Tachi <tachidito_7inc@hotmail.com>
2024-04-23 16:11:46 -0600

    new data

--- src/Components/Pages/GrafIndiApp.jsx
+++ src/Components/Pages/GrafIndiApp.jsx
@@ -9,7 +9,7 @@ export const GrafIndiApp = () => {
     setSelectedGroup(event.target.value);
   };
 
-  const selectedGroupData = updatedData.find(item => item.group === selectedGroup);
+  const selectedGroupData = updatedData.find(item => item.grupo === selectedGroup);
 
   return (
     <div className='container mt-5'>
@@ -27,13 +27,13 @@ export const GrafIndiApp = () => {
             style={{ marginBottom: '20px' }}
           >
             {updatedData.map((item) => (
-              <MenuItem key={item.group} value={item.group}>{item.group}</MenuItem>
+              <MenuItem key={item.grupo} value={item.grupo}>{item.grupo}</MenuItem>
             ))}
           </Select>
           <Grid container direction='row' alignItems='flex-end' rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
             {selectedGroupData && Object.entries(selectedGroupData).map(([key, value], index, array) => (
               <Grid item key={key} className='progressBar' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                {(key !== 'group' && index !== array.length - 1) && ( // Evita imprimir el último dato
+                {(key !== 'grupo' && index !== array.length - 1) && ( // Evita imprimir el último dato
                   <>
                     <LinearProgress color='error' variant="determinate" value={(value / 50) * 100} sx={{ width: '100%', height: 10, borderRadius: 1 }} />
                     <Typography variant='subtitle1' color='error' >{`${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`}</Typography>
--- src/Components/Pages/GraficaApp.jsx
+++ src/Components/Pages/GraficaApp.jsx
@@ -31,9 +31,9 @@ export const GraficaApp = () => {
         <CardContent>
           <Grid container direction='row' alignItems='flex-end' rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
             {rankSort.map((item) => (
-              <Grid item key={item.group} className='progressBar' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Grid item key={item.grupo} className='progressBar' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <LinearProgress color='error' variant="determinate" value={(item.points / 250) * 100} sx={{ width: '100%', height: 10, borderRadius: 1 }} />
-                <Typography variant='subtitle1' color='error' elevation={24}>{`${item.group}: ${item.points}`}</Typography>
+                <Typography variant='subtitle1' color='error' elevation={24}>{`${item.grupo}: ${item.points}`}</Typography>
               </Grid>
             ))}
           </Grid>
