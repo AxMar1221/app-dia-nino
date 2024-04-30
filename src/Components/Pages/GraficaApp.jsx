265d5070659c3ea43f79f926bbe4acf64d9cc36b
Tachi <tachidito_7inc@hotmail.com>
2024-04-30 14:40:21 -0600

    points update 3

--- src/Components/Pages/GraficaApp.jsx
+++ src/Components/Pages/GraficaApp.jsx
@@ -44,7 +44,7 @@ export const GraficaApp = () => {
           <Grid container direction='row' alignItems='flex-end' rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
             {rankSort.map((item) => (
               <Grid item key={item.grupo} className='progressBar' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <LinearProgress color='error' variant="determinate" value={(item.points / 250) * 100} sx={{ width: '100%', height: 15, borderRadius: 1 }} />
+                <LinearProgress color='error' variant="determinate" value={(item.points / 300) * 100} sx={{ width: '100%', height: 15, borderRadius: 1 }} />
                 <Typography variant='subtitle1' color='error' elevation={24}>{`${item.grupo} a ganado un total de "${item.points}" puntos `}</Typography>
               </Grid>
             ))}
